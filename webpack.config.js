846db56867ad5766c29950cdbe66be0bec42f947
Lucas Caires <lucas@Mac-mini-de-Lucas.local>
2018-01-13 21:18:21 -0200

    Add ES6 support with babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,18 @@ module.exports = {
 		path: path.resolve(__dirname, 'public')
 	},
 	module: {
-		rules: [{
+		rules: [
+		{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		},
+		{
 			test: /\.scss$/,
 			use: extractPlugin.extract({
 				use: ['css-loader', 'sass-loader']
